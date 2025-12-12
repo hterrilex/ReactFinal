a2c2ff4939c1269b8735c5398ceb5477e9e26515
Hugo <hterrile@gmail.com>
2025-12-12 18:41:25 -0300

    ultima actualizacion

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,20 +4,17 @@ function Inicio() {
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <h1>| Librería Atalaya |</h1>
+        <h1>| Disquera Old Money |</h1>
         <hr />
         <main>
           <p>
-            La <strong>Biblia</strong>, el libro más importante del mundo.
+            La música es lo único que puede salvar al mundo.
           </p>
           <p>
-            La Biblia está dividida en dos partes principales, el Antiguo Testamento y el Nuevo
-            Testamento. El Antiguo Testamento cubre el tiempo antes de Jesús, y el Nuevo Testamento
-            incluye la vida de Jesús y lo que sucede durante varias décadas después.
+
           </p>
           <img
-            src="https://i.pinimg.com/736x/ec/88/11/ec8811b38ef363dd661cf8d5fae18a2d--tapas-book-jacket.jpg"
-            alt="Tapa de Biblia"
+
           />
         </main>
       </div>
