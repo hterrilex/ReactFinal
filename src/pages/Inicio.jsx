d3b80d3ab472fbaf2c1927b49d2bafa231832984
Hugo <hterrile@gmail.com>
2025-12-13 01:56:13 -0300

    cambio 9

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -11,30 +11,30 @@ function Inicio() {
           <p style={{ whiteSpace: 'pre-line' }}>
             <strong>Pura suerte</strong>
             {`
-Que un sueño acabó, ya te dijeron,
-pero no, que todos los sueñitos, no.
-Arrugar no es ir al saladero,
-pero ¡ay!, mi viejo, ¡ay qué rabia da!
+              Que un sueño acabó, ya te dijeron,
+              pero no, que todos los sueñitos, no.
+              Arrugar no es ir al saladero,
+              pero ¡ay!, mi viejo, ¡ay qué rabia da!
 
-Yo no puedo librarme
-a lo que te debo como ilusión.
+              Yo no puedo librarme
+              a lo que te debo como ilusión.
 
-Si pudiera, como si fuera un chico,
-emborrachar el ritmo de un maldito rock.
+              Si pudiera, como si fuera un chico,
+              emborrachar el ritmo de un maldito rock.
 
-Pero hay un par de ojos inclaudicables
-que valió la pena alquilar para ver.
+              Pero hay un par de ojos inclaudicables
+              que valió la pena alquilar para ver.
 
-Imaginá los planes
-que en mi mente están, tan sin dolor.
+              Imaginá los planes
+              que en mi mente están, tan sin dolor.
 
-Esto ya
-esto ya no es rock, mi amor
-es pura suerte.
+              Esto ya
+              esto ya no es rock, mi amor
+              es pura suerte.
 
-Esto ya
-esto ya no es rock, mi amor
-es pura suerte.`}
+              Esto ya
+              esto ya no es rock, mi amor
+              es pura suerte.`}
           </p>
           <img 
             src="https://cdn.pixabay.com/photo/2018/10/09/19/56/music-3735836_1280.jpg"
