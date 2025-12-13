e32197ed4dd24f38ce87e221990e5e8ef92cd252
Hugo <hterrile@gmail.com>
2025-12-13 01:02:10 -0300

    cambio 5

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -11,9 +11,35 @@ function Inicio() {
             La música es lo único que puede salvar al mundo.
           </p>
           <p>
+            Pura suerte
+
+Que un sueño acabó, ya te dijeron,
+pero no, que todos los sueñitos, no.
+Arrugar no es ir al saladero,
+pero ¡ay!, mi viejo, ¡ay qué rabia da!
+
+Yo no puedo librarme
+a lo que te debo como ilusión.
+
+Si pudiera, como si fuera un chico,
+emborrachar el ritmo de un maldito rock.
+
+Pero hay un par de ojos inclaudicables
+que valió la pena alquilar para ver.
+
+Imaginá los planes
+que en mi mente están, tan sin dolor.
+
+Esto ya
+esto ya no es rock, mi amor
+es pura suerte.
+
+Esto ya
+esto ya no es rock, mi amor
+es pura suerte.
 
           </p>
-          <img
+          <img src="https://cdn.pixabay.com/photo/2018/10/09/19/56/music-3735836_1280.jpg"
 
           />
         </main>
